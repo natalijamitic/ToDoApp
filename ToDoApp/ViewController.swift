9c841962e3f52fb7f73f21d75b4b4ccd0fbf9188
Natalija Mitic <naca@ntec.ch>
2019-08-25 21:14:16 +0200

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Natalija Mitic on 25.08.19.
+//  Copyright © 2019 Natalija Mitic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
